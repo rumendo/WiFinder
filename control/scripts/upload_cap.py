433574ce9ddb2d9a10962f73d230569bcfbafb70
rumendo <rumen_do@abv.bg>
2019-01-31 03:28:42 +0200

    Added support for multiple devices

--- control/scripts/upload_cap.py
+++ control/scripts/upload_cap.py
@@ -1,10 +1,10 @@
 import requests
 
 
-cookie = {'key':'7fcb3ff5176fb532124b5ac3e4616a04'}
-
+cookie = {'key': '7fcb3ff5176fb532124b5ac3e4616a04'}
+file = 'home.cap'
 url = 'https://wpa-sec.stanev.org/?submit'
-files = {'file': open('home.cap', 'rb')}
+files = {'file': open(file, 'rb')}
 
 r = requests.post(url, files=files, cookies=cookie)
 print(r.content)
