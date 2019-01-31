530967998e5e0e7c3f4bd1abd1ea60eddc3b9a93
rumendo <rumen_do@abv.bg>
2019-01-31 23:48:49 +0200

    New devices added to db

--- control/views.py
+++ control/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from control.scripts.hcx_control import hcx_start
+import psycopg2
+
+hostname = 'localhost'
+username = 'controller'
+password = 'root'
+database = 'wifinder'
+psycopg2
 
 def index(request):
     return render(request, 'control/home.html')
